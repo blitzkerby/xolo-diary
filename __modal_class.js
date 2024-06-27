11837a2f0dc3718a44ab709c273df6f12450fc05
Admin-Kerby <hakkerby01@gmail.com>
2024-06-27 15:06:02 +0700

    MODIFIED : modal component

--- __modal_class.js
+++ __modal_class.js
@@ -54,21 +54,58 @@
 // const textarea = document.getElementById("notes");
 // localStorage.setItem('textareaValue', textarea.value);
 
-
 class NoteEditor {
-  constructor() {
+  constructor(id) {
+    this.UID = id
+    this.generateHTML();
+
     this.titleContainer = document.querySelector("name");
     this.notesContainer = document.querySelector("content");
+    this.dateContainer = document.querySelector("date")
+
     this.title = null;
     this.textarea = null;
+    this.date = null;
+
     this.initialize();
   }
 
   initialize() {
     this.createTitle();
     this.createTextarea();
+    this.createCalendar();
     this.loadSavedValue();
     this.setupEventListeners();
+  }
+
+  generateHTML(){
+    const notes = document.querySelector("notes")
+    notes.innerHTML = `
+    <div class="container-wrapper centered-x centered-y">
+        <div class="modal-container container">
+            <div class="header-container">
+                <name></name>
+            </div>
+            <div class="content-container">
+                <content></content>
+            </div>
+            <div class="date-container">
+                <date></date>
+                <!-- <p class="dm-mono-regular">
+                    2021-01-01
+                </p> -->
+                <!-- <input 
+                    class = "dm-mono-regular"
+                    type  = "date" 
+                    id    = "start" 
+                    name  = "trip-start" 
+                    value = "" 
+                    min   = "" 
+                    max   = "" /> -->
+            </div>
+        </div>
+    </div>
+    `;
   }
 
   createTitle() {
@@ -77,8 +114,7 @@ class NoteEditor {
         class           = "dm-mono-regular"
         id              = "title"
         contenteditable = "true"
-      >
-      </h1>
+      ></h1>
     `;
 
     this.titleContainer.innerHTML = name;
@@ -91,36 +127,38 @@ class NoteEditor {
         class       = "dm-mono-regular"
         id          = "notes"
         placeholder = "Enter text"
-      >
-      </textarea>
+      ></textarea>
     `;
     this.notesContainer.innerHTML = content;
     this.textarea = document.getElementById("notes");
   }
 
-  loadSavedValue() {
-    const savedTitle = localStorage.getItem("titleValue");
-    const savedValue = localStorage.getItem("textareaValue");
+  createCalendar() {
+    let dateHTML = `<p class="dm-mono-regular" id="date">27-06-1955</p>`
+    this.dateContainer.innerHTML = dateHTML
+    this.date = document.getElementById("date")
+  }
 
-    if (savedValue) {
-      this.textarea.value = savedValue;
-    }
-    if (savedTitle) {
-      this.title.innerText = savedTitle; // Update this line
-    } else {
-      this.title.innerText = "Untitled";
-    }
+  loadSavedValue() {
+    const savedTitle = localStorage.getItem(`${this.UID}-title`);
+    const savedValue = localStorage.getItem(`${this.UID}-textarea`);
+
+    this.title.innerText = savedTitle || "Untitled"; // Update this line
+    this.textarea.value = savedValue;
   }
 
   setupEventListeners() {
     this.textarea.addEventListener("input", () => {
-      localStorage.setItem("textareaValue", this.textarea.value);
+      localStorage.setItem(`${this.UID}-textarea`, this.textarea.value);
     });
 
     this.title.addEventListener("input", () => {
-      localStorage.setItem("titleValue", this.title.innerText);
+      localStorage.setItem(`${this.UID}-title`, this.title.innerText);
     });
   }
 }
 // Instantiate the class
-const noteEditor = new NoteEditor();
+
+export function LoadNoteEditor(id){
+  new NoteEditor(id);
+}
---
+++ modal.js
@@ -0,0 +1,6 @@
+import { LoadNoteEditor } from "./__modal_class.js";
+
+
+// pass in the UID of the note
+LoadNoteEditor("temporary-data-1")
+// LoadNoteEditor("temporary-data-2")
