d436661c8b2e1377ae373251f1190b4df0b60040
K3RBY <hakkerby01@gmail.com>
2024-06-29 17:51:56 +0700

    fix : responsiveness

--- .xolonotes/script.js
+++ .xolonotes/script.js
@@ -1,29 +1,6 @@
 import { loadHeader }       from "./components/header/header.mjs";
 import { loadMain }         from      "./templates/main/main.mjs";
-// import { createButton }     from  "./components/button/button.js";
+
 
 loadHeader();
 loadMain();
-
-// const buttons = {
-//     notes : {
-//         container    : 'main .container',
-//         btn_id    : 'notebook-btn',
-//         img_path  : "./.xolonotes/images/icons/notebook-white.svg",
-//         img_alt   : "notebook icon",
-//         img_secondary_path  : "./.xolonotes/images/icons/notebook-black.svg",
-//         img_secondary_alt   : "notebook secondary icon",
-//     },
-    
-//     calendar : {
-//         container : 'main .button-container',
-//         btn_id : 'calendar-btn',
-//         img_path : "./.xolonotes/images/icons/calendar-white.svg",
-//         img_alt : "calendar icon",
-//         img_secondary_path : "./.xolonotes/images/icons/calendar-black.svg",
-//         img_secondary_alt  : "calendar secondary icon",
-//     },
-// }
-
-// createButton(buttons.notes)
-// createButton(buttons.calendar)
