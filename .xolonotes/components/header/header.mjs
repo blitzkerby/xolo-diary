df35a4fc13e780fea9d5b0fa89211bc00ec89a9b
K3RBY <hakkerby01@gmail.com>
2024-07-03 20:43:36 +0700

    add : header feature github repository referal works

--- .xolonotes/components/header/header.mjs
+++ .xolonotes/components/header/header.mjs
@@ -1,30 +1,25 @@
 const header_assets = {
-  xolo_logo     : { id: "xolo-logo"  , className:"logo", alt: "xolo logo"  , path: "./.xolonotes/images/icons/xolo-logo.png" },
-  github_logo   : { id: "github-logo", className:"logo", alt: "github logo", path: "./.xolonotes/images/icons/github-logo.svg" },
-  page_text     : {
-    home    : { id : "home-text"   , alt: "home"   , className:"text-card", path: "./.xolonotes/images/icons/Home.svg" },
-    journal : { id : "jounral-text", alt: "journal", className:"text-card", path: "./.xolonotes/images/icons/Journal.svg"},
+  xolo_logo: { id: "xolo-logo", className: "logo", alt: "xolo logo", path: "./.xolonotes/images/icons/xolo-logo.png" },
+  github_logo: { id: "github-logo", className: "logo", alt: "github logo", path: "./.xolonotes/images/icons/github-logo.svg", link: "https://github.com/blitzkerby/xolo-diary.git" },
+  page_text: {
+    home: { id: "home-text", alt: "home", className: "text-card", path: "./.xolonotes/images/icons/Home.svg" },
+    journal: { id: "journal-text", alt: "journal", className: "text-card", path: "./.xolonotes/images/icons/Journal.svg" },
   }
 };
 
-
-
-function imgHTML (item)
-{
-    return `
+function imgHTML(item) {
+  return `
     <img 
-            class=${item.className}
-            src=${item.path} 
-            alt=${item.alt}
-            id =${item.id}
-        >
-    `;
+      class=${item.className}
+      src=${item.path} 
+      alt=${item.alt}
+      id=${item.id}
+    >
+  `;
 }
 
-const xoloHTML    = imgHTML(header_assets.xolo_logo);
-const githubHTML  = imgHTML(header_assets.github_logo);
-// const homeHTML    = imgHTML(header_assets.page_text.home);
-// const journalHTML = imgHTML(header_assets.page_text.journal);
+const xoloHTML = imgHTML(header_assets.xolo_logo);
+const githubHTML = imgHTML(header_assets.github_logo);
 
 export function loadHeader() {
   const header = document.querySelector("header");
@@ -38,7 +33,9 @@ export function loadHeader() {
     <ul class="nav-list">
       <li class="nav-item nav-logo">${xoloHTML}</li>
       <li class="nav-item nav-text">${pageImg}</li>
-      <li class="nav-item nav-logo">${githubHTML}</li>
+      <li class="nav-item nav-logo">
+        <a href="${header_assets.github_logo.link}" target="_blank">${githubHTML}</a>
+      </li>
     </ul>
     <div class="header-canvas left-shift"></div>
     <div class="header-canvas right-shift"></div>
